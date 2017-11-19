42c940661ac5104d02092505070ce4b507bf8e73
Jonathan Kerr <jonathandrewkerr@gmail.com>
2017-11-19 17:04:41 +0000

    testing 'remains alive' rule

--- test_gol.py
+++ test_gol.py
@@ -13,9 +13,16 @@ def test_given_a_live_cell_with_fewer_than_two_neighbours_when_tick_is_called_di
     assert live_cell.will_i_live() is False
 
 
-def test_give_a_live_cell_with_greater_than_three_neighbours_when_tick_is_called_dies(live_cell):
+def test_given_a_live_cell_with_greater_than_three_neighbours_when_tick_is_called_dies(live_cell):
     live_cell.live_neighbours = 4
     assert live_cell.will_i_live() is False
+
+
+def test_given_a_live_cell_with_two_or_three_neighbours_when_tick_is_called_remains_alive(live_cell):
+    live_cell.live_neighbours = 3
+    assert live_cell.will_i_live() is True
+    live_cell.live_neighbours = 2
+    assert live_cell.will_i_live() is True
 
 
 
